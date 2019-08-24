e75784bec6614cc2f7af1815716fa9eb3bcc8c99
Sam Squire <sam@samsquire.com>
2019-08-24 14:36:32 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          new server <code>src/App.js</code> and save to reload.
+          Redeploy <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
