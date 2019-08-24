a6d22d587870949f0c461a72cd988c1a5da8b4e1
Sam Squire <sam@samsquire.com>
2019-08-24 15:54:19 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 
 class MyThing extends React.Component {
   constructor(props) {
