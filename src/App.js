21dd6eb50087b91622e77d48c71283eae5391041
Sam Squire <sam@samsquire.com>
2019-08-24 15:57:17 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class MyThing extends React.Component {
 function App() {
   return (
     <div className="App">
-	<Navbar bg="light" expand="lg">
+	<Navbar bg="light" expand="lg" fixed="top">
   <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
