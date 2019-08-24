f7a815c626b46a172513b1c2f573adb9641e9ea4
Sam Squire <sam@samsquire.com>
2019-08-24 18:59:41 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -39,7 +39,6 @@ class MyThing extends React.Component {
 
 function App() {
   return (
-   
 	<div className="App">
 	<Navbar bg="light" expand="lg" fixed="top" sticky="top">
   <Navbar.Brand href="#home">Sam Squire</Navbar.Brand>
