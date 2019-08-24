d0c3114e1c96af33315e5592542e73db2b3dd73b
Sam Squire <sam@samsquire.com>
2019-08-24 18:59:09 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Card from 'react-bootstrap/Card';
 
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import ideaApp from './reducers';
-import Provider from 'react-redux/Provider';
 
 class MyThing extends React.Component {
   constructor(props) {
