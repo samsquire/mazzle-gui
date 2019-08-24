5c332ee86aaf72a526dc606d07faa3cc45d31394
Sam Squire <sam@samsquire.com>
2019-08-24 19:21:11 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ import FormControl from 'react-bootstrap/FormControl';
 import Card from 'react-bootstrap/Card';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import rootReducer from './reducers/reducers'
 
 class MyThing extends React.Component {
   constructor(props) {
