cf8c2a2b10c2bd5e3018f05bfc0701e6c49a0275
Sam Squire <sam@samsquire.com>
2019-08-24 16:07:37 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -59,6 +59,18 @@ function App() {
   </Navbar.Collapse>
 </Navbar>
 	
+	<Card style={{ width: '18rem' }}>
+ 
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+	
       <header className="App-header">
       
         <p>
