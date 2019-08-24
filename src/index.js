57fd75b6799d4b5c450a1deb9defedd3c0ee7e8e
Sam Squire <sam@samsquire.com>
2019-08-24 19:31:10 +0100

    autocommit experimenting with detections

--- src/index.js
+++ src/index.js
@@ -4,13 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
-
-ReactDOM.render(<Provider store={store}><App />,</Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
