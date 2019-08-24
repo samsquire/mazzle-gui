fcecac2bc87f3e54b83a5faee8d37a39d304ab5d
Sam Squire <sam@samsquire.com>
2019-08-24 16:09:25 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
-import Card from 'react-bootstrap/Card';
+import Card from 'react-bootstrap/FormControl';
 
 class MyThing extends React.Component {
   constructor(props) {
@@ -71,31 +71,6 @@ function App() {
     <Button variant="primary">Go somewhere</Button>
   </Card.Body>
 </Card>
-
-	<Card style={{ width: '18rem' }}>
- 
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-
-	<Card style={{ width: '18rem' }}>
- 
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-
 	
       <header className="App-header">
       
