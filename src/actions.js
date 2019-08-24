39563c9246939246f3cca7425fac3354191f39a8
Sam Squire <sam@samsquire.com>
2019-08-24 18:15:22 +0100

    autocommit experimenting with detections

--- src/actions.js
+++ src/actions.js
@@ -1,12 +1,13 @@
-const ADD_IDEA = 'ADD_IDEA';
-const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
-const VISIBIlTY_FILTERS = {
+export const ADD_IDEA = 'ADD_IDEA';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+export const VISIBIlTY_FILTERS = {
 	SHOW_ALL: 'SHOW_ALL'
 }
 
-function addIdea(text) {
+export function addIdea(text) {
 	return {
 		type: ADD_IDEA,
 		text: text
 	};
 }
+
