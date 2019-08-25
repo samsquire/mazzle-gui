752aecdee3ca244a7f75ff2eb2cdaf9e35dd53ad
Sam Squire <sam@samsquire.com>
2019-08-25 19:50:19 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
     </Nav>
- <Form>
+ <Form inline>
  <Form.Group controlId="formBasicEmail">
     <Form.Control inline type="email" placeholder="Enter email" />
     <Form.Control inline type="password" placeholder="Password" />
