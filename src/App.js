f2306a1819388a75146cfb28043c21b1085bac45
Sam Squire <sam@samsquire.com>
2019-08-25 19:45:27 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -54,10 +54,22 @@ function App() {
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
     </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
+
+ <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+
+
+
   </Navbar.Collapse>
 </Navbar>
 <Breadcrumb>
