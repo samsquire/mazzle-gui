ccd4f7791013eb360d9a28f0718a0dcda08ed271
Sam Squire <sam@samsquire.com>
2019-09-03 17:37:05 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -64,6 +64,7 @@ class MyThing extends React.Component {
 
 function App() {
   return (
+  <div className="App">
    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
       <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
       <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
@@ -300,7 +301,7 @@ function App() {
         </main>
       </div>
     </div>
-	
+	</div>
 	
 	
   );
