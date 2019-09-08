3cb822f0d52872a8daf6112818bef98c0db0525f
Sam Squire <sam@samsquire.com>
2019-09-08 22:30:36 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -203,7 +203,7 @@ function App() {
 		  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 		<h2 class="h2">Components</h2>
 		<div class="btn-toolbar mb-2 mb-md-0">
-			<Form.Control type="text" placeholder="Search" />
+			<Form.Control type="text" placeholder="filter" />
 			</div>
          </div>
 		 
