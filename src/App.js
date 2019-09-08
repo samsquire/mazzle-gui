7c5b7cd4309ad494eea096a195bb48dee7a7f6fe
Sam Squire <sam@samsquire.com>
2019-09-08 23:41:42 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -287,7 +287,11 @@ function App() {
 		<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 			<h2 class="h2">Task Artifacts</h2>
         </div>
-          
+         
+		 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+			<h2 class="h2">Broken</h2>
+        </div>
+		  
           <div class="table-responsive">
            
           </div>
