a5f961d85f172a079e5a67bc99073ae501d0ded4
Sam Squire <sam@samsquire.com>
2019-09-08 22:33:24 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -214,7 +214,8 @@ function App() {
         </div>
 		
 		<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-			<h2 class="h2">Component View</h2>
+			<h2 class="h2">Pipeline View</h2>
+			
         </div>
 		
 		<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
