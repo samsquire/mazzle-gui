bcb141e25a09471852991e26df478715a5777fae
Sam Squire <sam@samsquire.com>
2019-09-08 21:29:55 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -173,10 +173,23 @@ function App() {
 		<h2 class="h2">Environment</h2>
          </div>
 		 
-		</div>
+	
 		  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 		<h2 class="h2">Components</h2>
          </div>
+		 
+		 <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
 
           
           <div class="table-responsive">
