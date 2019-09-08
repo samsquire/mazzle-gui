f9a7c1667c90109116090b5fb665539e8d24d3d8
Sam Squire <sam@samsquire.com>
2019-09-08 21:27:03 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -156,7 +156,7 @@ function App() {
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 class="h2">Dashboard</h1>
-			<h2 class="h2">Environment</h2>
+			
             <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
                 <button class="btn btn-sm btn-outline-secondary">Share</button>
@@ -167,6 +167,7 @@ function App() {
                 This week
               </button>
             </div>
+			<h2 class="h2">Environment</h2>
           </div>
 
          
