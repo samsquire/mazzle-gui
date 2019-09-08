c6e46d586fdc9435d793641c234e14d325b34c4f
Sam Squire <sam@samsquire.com>
2019-09-08 21:27:47 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -167,10 +167,11 @@ function App() {
                 This week
               </button>
             </div>
-			<h2 class="h2">Environment</h2>
+			
           </div>
-
-         
+		  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+		<h2 class="h2">Environment</h2>
+         </div>
 
           
           <div class="table-responsive">
