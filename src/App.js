71f10ca96b18d989c5a61514087fe69d0ecb701d
Sam Squire <sam@samsquire.com>
2019-09-08 23:10:47 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ class LatestComponentStatus extends React.Component {
 		</Card>);
 		});
 		
-		var chunks = chunk(items, 10);
+		var chunks = chunk(items, 6);
 		var rows = chunks.map((item, index) => {
 			return (<Row>
 			{ item.map((component, index) => {return (<Col className="pl-0 pr-0">{component}</Col>); })} 
