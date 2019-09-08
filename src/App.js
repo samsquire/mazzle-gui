632f4780743a97650ac1fd68d37f24dad72476e8
Sam Squire <sam@samsquire.com>
2019-09-08 21:29:32 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -173,7 +173,7 @@ function App() {
 		<h2 class="h2">Environment</h2>
          </div>
 		 
-		</div>
+	
 		  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 		<h2 class="h2">Components</h2>
          </div>
