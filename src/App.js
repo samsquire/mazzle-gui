decfa092f4af88733458bc7ebfd77d7086966525
Sam Squire <sam@samsquire.com>
2019-09-08 22:25:08 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ function App() {
     <div class="container-fluid">
       <div class="row">
         <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
+          <div class="sidebar-sticky pt-3">
             <ul class="nav flex-column">
               <li class="nav-item">
                 <a class="nav-link active" href="#">
