6df5a90446d075d80e61b435733c31759ec22649
Sam Squire <sam@samsquire.com>
2019-09-08 22:10:00 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -207,7 +207,7 @@ function App() {
             </ul>
           </div>
         </nav>
-<ComponentList components={components} />
+
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 class="h2">Dashboard</h1>
@@ -226,7 +226,6 @@ function App() {
           </div>
 		  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 		<h2 class="h2">Environment</h2>
-		
          </div>
 		 
 	
@@ -238,12 +237,11 @@ function App() {
 
           
           <div class="table-responsive">
-           
+           <ComponentList components={components} />
           </div>
         </main>
       </div>
     </div>
-	
 	</div>
 	
 	
