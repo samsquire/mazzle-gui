93e16c36c7be21597ec085c2f6019ce62546638b
Sam Squire <sam@samsquire.com>
2019-09-09 22:12:05 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -324,7 +324,7 @@ class App extends React.Component {
 			  <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 				<h1 className="h2">Dashboard</h1>
 				
-				{JSON.stringify(this.props.store.getState(), 4)}
+				
 				
 				<div className="btn-toolbar mb-2 mb-md-0">
 				  <div className="btn-group mr-2">
@@ -338,6 +338,9 @@ class App extends React.Component {
 				</div>
 				
 			  </div>
+			  
+			  {JSON.stringify(this.props.store.getState(), 4)}
+			  
 			  <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 			<h2 className="h2">Environment</h2>
 			 </div>
