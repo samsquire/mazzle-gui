9626b26f28018df37ae18b849404c16cc92ac8ca
Sam Squire <sam@samsquire.com>
2019-09-09 21:40:05 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -112,7 +112,7 @@ class ComponentList extends React.Component {
 			}
 			
 			return (
-		 <Card className="mb-4" style={{ width: '15rem' }}>
+		 <Card key={item.name} className="mb-4" style={{ width: '15rem' }}>
 		  <Card.Body>
 			<Card.Title><ProgressBar variant={variant} {...progressAttributes} now="100" />{ item.name }</Card.Title>
 			<Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
