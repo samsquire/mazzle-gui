9e6cf349806471eb6150405202326cc0cba35372
Sam Squire <sam@samsquire.com>
2019-09-09 15:56:10 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -270,6 +270,8 @@ function App() {
 		<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 			<h2 class="h2">Task View</h2>
         </div>
+		<h3>terraform/app/test</h3>
+		
 		
 		<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 			<h2 class="h2">Component Build View</h2>
