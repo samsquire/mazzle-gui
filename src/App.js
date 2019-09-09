6e16094894b175ce6f3f595c0608148ed946da33
Sam Squire <sam@samsquire.com>
2019-09-09 21:39:22 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -264,51 +264,51 @@ class App extends React.Component {
 	  <div className="App">
 	   <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
 		  <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">devops-pipeline</a>
-		  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
-		  <ul class="navbar-nav px-3">
-			<li class="nav-item text-nowrap">
-			  <a class="nav-link" href="#">Sign out</a>
+		  <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
+		  <ul className="navbar-nav px-3">
+			<li className="nav-item text-nowrap">
+			  <a className="nav-link" href="#">Sign out</a>
 			</li>
 		  </ul>
 		</nav>
 
-		<div class="container-fluid">
-		  <div class="row">
-			<nav class="col-md-2 d-none d-md-block bg-light sidebar">
-			  <div class="sidebar-sticky pt-3">
-				<ul class="nav flex-column">
-				  <li class="nav-item">
-					<a class="nav-link active" href="#">
+		<div className="container-fluid">
+		  <div className="row">
+			<nav className="col-md-2 d-none d-md-block bg-light sidebar">
+			  <div className="sidebar-sticky pt-3">
+				<ul className="nav flex-column">
+				  <li className="nav-item">
+					<a className="nav-link active" href="#">
 					  <span data-feather="home"></span>
-					  Dashboard <span class="sr-only">(current)</span>
+					  Dashboard <span className="sr-only">(current)</span>
 					</a>
 				  </li>
-				  <li class="nav-item">
-					<a class="nav-link" href="#">
+				  <li className="nav-item">
+					<a className="nav-link" href="#">
 					  <span data-feather="file"></span>
 					  Components
 					</a>
 				  </li>
-				  <li class="nav-item">
-					<a class="nav-link" href="#">
+				  <li className="nav-item">
+					<a className="nav-link" href="#">
 					  <span data-feather="shopping-cart"></span>
 					  Environments
 					</a>
 				  </li>
-				  <li class="nav-item">
-					<a class="nav-link" href="#">
+				  <li className="nav-item">
+					<a className="nav-link" href="#">
 					  <span data-feather="users"></span>
 					  Tasks
 					</a>
 				  </li>
-				  <li class="nav-item">
-					<a class="nav-link" href="#">
+				  <li className="nav-item">
+					<a className="nav-link" href="#">
 					  <span data-feather="bar-chart-2"></span>
 					  Jobs
 					</a>
 				  </li>
-				  <li class="nav-item">
-					<a class="nav-link" href="#">
+				  <li className="nav-item">
+					<a className="nav-link" href="#">
 					  <span data-feather="layers"></span>
 					  Tooling
 					</a>
@@ -320,76 +320,76 @@ class App extends React.Component {
 			  </div>
 			</nav>
 
-			<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-			  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-				<h1 class="h2">Dashboard</h1>
+			<main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+			  <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+				<h1 className="h2">Dashboard</h1>
 				
-				<div class="btn-toolbar mb-2 mb-md-0">
-				  <div class="btn-group mr-2">
-					<button class="btn btn-sm btn-outline-secondary">Share</button>
-					<button class="btn btn-sm btn-outline-secondary">Export</button>
+				<div className="btn-toolbar mb-2 mb-md-0">
+				  <div className="btn-group mr-2">
+					<button className="btn btn-sm btn-outline-secondary">Share</button>
+					<button className="btn btn-sm btn-outline-secondary">Export</button>
 				  </div>
-				  <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
+				  <button className="btn btn-sm btn-outline-secondary dropdown-toggle">
 					<span data-feather="calendar"></span>
 					This week
 				  </button>
 				</div>
 				
 			  </div>
-			  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-			<h2 class="h2">Environment</h2>
+			  <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+			<h2 className="h2">Environment</h2>
 			 </div>
 			 <h3>Home</h3>
 		
-			  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-			<h2 class="h2">Components</h2>
-			<div class="btn-toolbar mb-2 mb-md-0">
+			  <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+			<h2 className="h2">Components</h2>
+			<div className="btn-toolbar mb-2 mb-md-0">
 				<Form.Control type="text" placeholder="Component" />
 				</div>
 			 </div>
 			 
 			<ComponentList components={this.props.store.getState().app.components} />
 			 
-			<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-				<h2 class="h2">Component View</h2>
+			<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+				<h2 className="h2">Component View</h2>
 			</div>
 			
 			<LatestComponentStatus latest={this.props.store.getState().app.latest} />
 			
-			<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-				<h2 class="h2">Pipeline View</h2>
+			<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+				<h2 className="h2">Pipeline View</h2>
 			</div>
 			
 			<EnvironmentPipeline pipeline={this.props.store.getState().app.pipeline} />
 			
-			<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-				<h2 class="h2">Task View</h2>
+			<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+				<h2 className="h2">Task View</h2>
 			</div>
 			<h3>terraform/app/test</h3>
 			
 			
-			<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-				<h2 class="h2">Component Build View</h2>
+			<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+				<h2 className="h2">Component Build View</h2>
 			</div>
 			 
-			<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-				<h2 class="h2">Task Outputs</h2>
+			<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+				<h2 className="h2">Task Outputs</h2>
 			</div>
 
-			<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-				<h2 class="h2">Task Log File</h2>
+			<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+				<h2 className="h2">Task Log File</h2>
 			</div>
 			
 			
-			<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-				<h2 class="h2">Task Artifacts</h2>
+			<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+				<h2 className="h2">Task Artifacts</h2>
 			</div>
 			 
-			 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-				<h2 class="h2">Broken</h2>
+			 <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+				<h2 className="h2">Broken</h2>
 			</div>
 			  
-			  <div class="table-responsive">
+			  <div className="table-responsive">
 			   
 			  </div>
 			</main>
