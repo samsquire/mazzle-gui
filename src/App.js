4d8b21d2e7df9d5d6bb0a7d3818159c1110ef96f
Sam Squire <sam@samsquire.com>
2019-09-09 21:39:00 +0100

    autocommit experimenting with detections

--- src/App.js
+++ src/App.js
@@ -262,7 +262,7 @@ class App extends React.Component {
 	render() {
 	  return (
 	  <div className="App">
-	   <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+	   <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
 		  <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">devops-pipeline</a>
 		  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
 		  <ul class="navbar-nav px-3">
